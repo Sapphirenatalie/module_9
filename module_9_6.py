1b606fcd12ca78e123b870b6ce5c4dde8e2e57b1
Sapphirenatalie <168541642+Sapphirenatalie@users.noreply.github.com>
2024-07-17 23:38:43 +0300

    Add files via upload
    Генераторы

---
+++ module_9_6.py
@@ -0,0 +1,34 @@
+# Генераторы
+
+# Напишите функцию-генератор all_variants(text),
+# которая принимает строку text и возвращает объект-генератор,
+# при каждой итерации которого будет возвращаться подпоследовательности переданной строки.
+
+# Пункты задачи:
+# Напишите функцию-генератор all_variants(text).
+# Опишите логику работы внутри функции all_variants.
+# Вызовите функцию all_variants и выполните итерации.
+# Пример результата выполнения программы:
+# Пример работы функции:
+# a = all_variants("abc")
+# for i in a:
+# print(i)
+
+
+def all_variants(text):
+    for symbol_x in range(len(text)):
+        for symbol_y in range(len(text) - symbol_x):
+            yield text[symbol_y:symbol_y + symbol_x + 1]
+
+
+for value in all_variants("abc"):
+    print(value)
+
+
+# Вывод на консоль:
+# a
+# b
+# c
+# ab
+# bc
+# abc
